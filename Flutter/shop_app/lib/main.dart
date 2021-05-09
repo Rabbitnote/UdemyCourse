0fc12487486854d0e89e361f3413bb6cbc58cfc3
note <note16651@hotmail.co.th>
2021-05-09 22:02:08 +0700

    [SHOP APP] ADD Provider

--- Flutter/shop_app/lib/main.dart
+++ Flutter/shop_app/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import './screens/product_overview_screen.dart';
+import './screens/product_detial_screen.dart';
+import './provider/products.dart';
 void main() {
   runApp(MyApp());
 }
@@ -8,24 +12,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+    return ChangeNotifierProvider(
+          create: (ctx)=> Products() ,
+          child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(primarySwatch: Colors.purple,accentColor: Colors.deepOrange,fontFamily: 'Lato'),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+
+        },
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a shop '),
-      ),
-    );
-  }
-}
---
+++ Flutter/shop_app/lib/screens/product_detial_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../provider/products.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  // final double price;
+  // ProductDetailScreen(this.title,this.price);
+  static const routeName = '/product-detial';
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context,listen: false).findById(productId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
+  }
+}
