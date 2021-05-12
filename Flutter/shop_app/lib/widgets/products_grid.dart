b9c5b88e7fb90f81ab336d1272ce8b3bc5a7e8c1
note <note16651@hotmail.co.th>
2021-05-12 17:19:30 +0700

    [SHOPAPP] Learning use provider

--- Flutter/shop_app/lib/widgets/products_grid.dart
+++ Flutter/shop_app/lib/widgets/products_grid.dart
@@ -3,21 +3,26 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/provider/products.dart';
 import './product_item.dart';
 
-class ProductsGrid extends StatelessWidget{
+class ProductsGrid extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
     final products = productsData.items;
     return GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: products.length,
-        itemBuilder: (ctx, i) => ProductItem(
-            products[i].id, products[i].title, products[i].imageUrl),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10),
-      );
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value :products[i],
+        // create: (c) => products[i],
+        child: ProductItem(
+            // products[i].id, products[i].title, products[i].imageUrl
+            ),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+    );
   }
 }
