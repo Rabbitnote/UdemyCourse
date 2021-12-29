d22074def25f8890e1360a0774bd24dfa6ce71ff
note <note16651@hotmail.co.th>
2021-12-29 14:24:01 +0700

    [Update] Before delpoy

--- FullStack-React/server/index.js
+++ FullStack-React/server/index.js
@@ -5,7 +5,7 @@ const app = express(); //Running express that we register | app this for setup c
 app.get('/',(req,res)=>{
     res.send({hi:'there'})
 });
-
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
 
 
